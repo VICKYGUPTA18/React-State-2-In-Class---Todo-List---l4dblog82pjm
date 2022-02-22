b57f7fcc6488a40110b26aef64f8b9837d833bec
VICKYGUPTA18 <96422912+VICKYGUPTA18@users.noreply.github.com>
2022-02-23 02:42:19 +0530

    sv

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+  const [name, setName] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChangeHandler = (e) => {
+    setName(e.target.value);
+  };
+  const onAdd = () => {
+    setTodos([...todos, { id: Math.random() * 100, name: name }]);
+    setName("");
+  };
+  const onDelete = (e) => {
+    setTodos(todos.filter((el) => el.id !== e.target.id));
+  };
+  return (
+    <div id="main">
+      <div>
+        <input onChange={onChangeHandler} value={name} />
+        <button id="btn" className="btn-click" onClick={onAdd}>
+          add
+        </button>
+      </div>
+      {todos.map((todo) => {
+        return (
+          <>
+            <textarea id="task" key={todo.id}>
+              {todo.name}
+            </textarea>
+            <button type="button" onClick={(e) => onDelete(e)} id={todo.id}>
+              Delete
+            </button>
+          </>
+        );
+      })}
+    </div>
+  );
 }
-
 
 export default App;
